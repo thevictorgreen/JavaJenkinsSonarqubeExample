eff5c6f5bb68c03c9d1b31ac7053ec53beebf777
thevictorgreen <thevictorgreen@gmail.com>
2017-09-07 00:48:40 -0400

    added files

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+//Test.java
+package com.thevictorgreen;
+import java.io.*;
+import java.util.*;
+
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println("Success!");
+  }
+  
+}
